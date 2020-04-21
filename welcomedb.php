e3069b7b2ada5e6919312999bfb2413abd824f94
sephora-k <g00362884@gmit.ie>
2020-04-21 10:37:46 +0000

    LOGIN Pages

---
+++ welcomedb.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+//if (!isset(($_SESSION['username'])){
+	//header('location:login_db.php');}
+	
+	echo "<a href='../db_project/homepage.html'> Go to Home Page</a>";
+?>
+
+<title> User Login and Registration </title>
+	<link rel="stylesheet" type ="text/css" href="style_db.css">
+		<link rel="stylesheet" type="text/css"
+		href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+<html>
+<head>
+<title> Home Page</title>
+</head>
+
+<body>
+
+<div class ="container">
+
+<a class="float-right" href="logoutdb.php"> LOGOUT </a>
+
+<h1> Welcome <?php echo $_SESSION['username']; ?> </h1>
+
+</div>
+
+</body>
+
+</html>
